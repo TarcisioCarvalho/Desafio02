8d0e79af74e607913d41eb9be2b8720908d7f20e
Tarcísio José de Amorim Carvalho <tarcisiokta2012@gmail.com>
2021-07-25 10:39:27 -0300

    Desafio02

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// Cliente
 let nome = document.querySelector('#nome');
 let labelNome = document.querySelector('#labelNome');
 let validnome=false;
@@ -10,6 +12,41 @@ let tel= document.querySelector('#tel');
 let labelTel = document.querySelector('#labelTel');
 let validtel=false;
 
+// Endereço
+
+let estado = document.querySelector('#estado');
+let labelEstado = document.querySelector('#labelEstado');
+let validestado=false;
+
+let cidade = document.querySelector('#cidade');
+let labelCidade = document.querySelector('#labelCidade');
+let validcidade=false;
+
+let rua = document.querySelector('#rua');
+let labelRua = document.querySelector('#labelRua');
+let validrua=false;
+
+let bairro = document.querySelector('#bairro');
+let labelBairro = document.querySelector('#labelBairro');
+let validbairro=false;
+
+let cep = document.querySelector('#cep');
+let labelCep = document.querySelector('#labelCep');
+let validcep=false;
+
+let numero = document.querySelector('#numero');
+let labelNumero = document.querySelector('#labelNumero');
+let validnumero=false;
+
+
+
+
+
+
+
+
+
+//Produto
 let nomeProd = document.querySelector('#nomeProd');
 let labelNomeProd = document.querySelector('#labelNomeProd');
 let validnomeProd=false;
@@ -26,7 +63,7 @@ let quantidade = document.querySelector('#quantidade');
 let labelQuantidade = document.querySelector('#labelQuantidade');
 let validquantidade=false;
 
-nome.addEventListener('keyup',()=>{
+/*nome.addEventListener('keyup',()=>{
     if(nome.value.length <=2){
         labelNome.setAttribute('style','color:red');
         labelNome.innerHTML = '<strong> Nome *Insira no minímo 3 caracteres</strong>' 
@@ -36,23 +73,82 @@ nome.addEventListener('keyup',()=>{
         labelNome.innerHTML = 'Nome'
         nome.setAttribute('style' , 'border-color:green');
     }
-})
+})*/
+
+
+nome.addEventListener('keyup',()=>{
+    nome.value? validnome = true : validnome = false;
+});
+
+email.addEventListener('keyup',()=>{
+    email.value? validemail = true : validemail = false;
+});
+tel.addEventListener('keyup',()=>{
+    tel.value? validtel = true : validtel = false;
+});
+
+cidade.addEventListener('keyup',()=>{
+    cidade.value? validcidade = true : validcidade = false;
+});
+estado.addEventListener('keyup',()=>{
+    estado.value? validestado = true : validestado = false;
+});
+rua.addEventListener('keyup',()=>{
+    rua.value? validrua = true : validrua = false;
+});
+cep.addEventListener('keyup',()=>{
+    cep.value? validcep = true : validcep = false;
+});
+numero.addEventListener('keyup',()=>{
+    numero.value? validnumero = true : validnumero = false;
+});
+bairro.addEventListener('keyup',()=>{
+    bairro.value? validbairro = true : validbairro= false;
+});
+
+
+nomeProd.addEventListener('keyup',()=>{
+    nomeProd.value? validnomeProd = true : validnomeProd = false;
+});
+preco.addEventListener('keyup',()=>{
+    preco.value? validpreco = true : validpreco = false;
+});
+tipo.addEventListener('keyup',()=>{
+    tipo.value? validtipo = true : validtipo = false;
+});
+quantidade.addEventListener('keyup',()=>{
+    quantidade.value? validquantidade = true : validquantidade= false;
+});
+
+
 
 function cadastrar() {
+    if(validnome && validtel && validemail && validestado && validcidade && validnumero && validrua 
+        && validbairro && validcep){
    listaClientes = JSON.parse(localStorage.getItem('listaClientes') || '[]')
    listaClientes.push(
        {
            nome:nome.value,
            tel:tel.value,
-           email:email.value
+           email:email.value,
+           estado:estado.value,
+           cidade:cidade.value,
+           bairro:bairro.value,
+           rua:rua.value,
+           cep:cep.value,
+           numero:numero.value,
        }
    )
    console.log(listaClientes);
 
    localStorage.setItem('listaClientes',JSON.stringify(listaClientes))
+}else{
+    alert('Preencha todos os espaços');
+}
 }
 
 function cadastrarProduto() {
+    if(validnomeProd && validpreco && validtipo && validquantidade){
     listaProdutos = JSON.parse(localStorage.getItem('listaProdutos') || '[]')
     listaProdutos.push(
         {
@@ -64,4 +160,7 @@ function cadastrarProduto() {
     )
  
     localStorage.setItem('listaProdutos',JSON.stringify(listaProdutos))
+}else{
+    alert('Preencha todos os espaços');
+}
  }
